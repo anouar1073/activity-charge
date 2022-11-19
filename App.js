92e31d15c5155e83e614e45d30e43d1723247d2d
Christian Bernhard <christianbernhard@w212-1p-v4.eduroam.dynamic.rbg.tum.de>
2022-11-19 20:42:14 +0100

    added map

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
+
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 48.135124,
+    longitude: 11.581981,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  })
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to startyour app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map} 
+        region={mapRegion}
+      >
+        <Marker coordinate={mapRegion} title='Marker' />
+      </MapView>
     </View>
   );
 }
@@ -13,8 +27,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
