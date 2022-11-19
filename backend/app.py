70c054b5ea16bd839f9ae2abc7a9ccc53b0e6afd
Florian Burger <F.burger@campus.lmu.de>
2022-11-19 20:47:50 +0100

    merge

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,51 @@ from flask import Flask
 import requests
 import json
 
-from backend.attractionAPI import get_attractions
 
 app = Flask(__name__)
 
 # Flask run
+
+def get_attractions(latitude: int, longitude: int):
+    apiKey = "AIzaSyBYcHDCj5i_pP2M5s37MbiQRMHNdRyJy6U"
+    latitude = "48.137154" 
+    longitude = "11.576124"
+    location = latitude + "%2C" + longitude
+
+    searchRadius = "50000" # in meters / max 50 000
+    attractionType = "tourist_attraction" # gym / amusement_park / supermarket /   #https://developers.google.com/maps/documentation/places/web-service/supported_types
+    keyword = "" # like google search
+
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + location + "&radius=" + searchRadius + "&type="+ attractionType + "&keyword=" + keyword + "&key=" + apiKey
+
+    payload={}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+
+    responseDict = json.loads(response.text)
+    with open('backend/attractions.json', 'w') as f:
+        json.dump(responseDict, f, indent=4)
+    
+    print(responseDict)
+    
+    attractionList = []
+    for dict in responseDict["results"]:
+        attraction = {
+            "AttractionName": "test",
+            "AttractionLocation": "test",
+            "AttractionDescription": "test",
+            "ChargerLocation": "test"
+            
+            # attractionImageURL
+            # proposedCharingTime
+            # chargePercentAfterCharing
+            # moneySavedFromDoNotGetPenalty  
+        }
+        attractionList.append(attraction)
+
+    print(attractionList)
+
 
 def fetchChargingStation(latitude: float, longitude: float):
     params = {"latitude": 48.252954, "longitude": 11.656477, "countrycode": "DE", 
@@ -38,10 +78,13 @@ def get_feed(latitude: float, longitude: float):
 
     latitude = "48.137154" 
     longitude = "11.576124"
+    # returns a list 
+    # "AttractionName": "test",
+    # "AttractionLocation": "test",
+    # "AttractionDescription": "test",
+    # "ChargerLocation": "test"
     attractionList = get_attractions(latitude=latitude, longitude=longitude)
-    # dsf
-
-    # list cleaner 
+    #fetchChargingStation(attractionList[])
 
 
     return '<h1>Hello from Flask & Docker</h2>'
